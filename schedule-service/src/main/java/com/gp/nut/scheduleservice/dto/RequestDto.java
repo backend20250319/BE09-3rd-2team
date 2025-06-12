f2a0c1bc20f360426cdc9f68f0e629d1948d5a8e
hyeblee <hyeb1224@naver.com>
2025-06-12 16:29:04 +0900

    feat: 회식 정보 요청을 위한 RequestDto 생성

---
+++ schedule-service/src/main/java/com/gp/nut/scheduleservice/dto/RequestDto.java
@@ -0,0 +1,35 @@
+package com.gp.nut.scheduleservice.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RequestDto {
+
+  @NotNull(message = "회식을 주최하는 사장 ID는  필수입니다.")
+  private Long bossId;
+
+  @NotNull(message = "회식 날짜는 필수입니다.")
+  private LocalDate dinnerDate;
+
+  @NotEmpty(message = "참여자 ID 목록은 비어있을 수 없습니다.") // null과 빈 리스트를 실패 처리
+  private List<Long> participantIds;
+
+  private List<Long> candidateLocationIds;
+
+  private Long confirmedLocationId; // 확정 장소는 없어도 회식 생성 가능
+
+}
