a98573427e51eef416cc95dce797a491536f0f62
juhui <juya202106@naver.com>
2025-06-12 16:00:46 +0900

    feat: Location Repository 추가

--- location-service/src/main/java/com/gp/nut/repository/LocationRepository.java
+++ location-service/src/main/java/com/gp/nut/repository/LocationRepository.java
@@ -1,4 +1,8 @@
 package com.gp.nut.repository;
 
-public class LocationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LocationRepository extends JpaRepository<Location, Long> {
 }
