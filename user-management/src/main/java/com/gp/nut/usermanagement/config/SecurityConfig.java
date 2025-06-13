3233fa8cf96c3bacd16d45bab4c3eb0a93217b7e
heechae <chaehee15@naver.com>
2025-06-13 09:58:13 +0900

    fix: 회원가입 기능 완료

--- user-management/src/main/java/com/gp/nut/usermanagement/config/SecurityConfig.java
+++ user-management/src/main/java/com/gp/nut/usermanagement/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.gp.nut.usermanagement.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +20,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth ->
+                    auth.requestMatchers(HttpMethod.POST, "/register").permitAll()
+                        .anyRequest().authenticated()
+                    );
+        return http.build();
     }
 }
