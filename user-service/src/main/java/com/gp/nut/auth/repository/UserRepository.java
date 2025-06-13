4c44dec7ec3e4cbbccba851cfc05a40241868709
FerryLa <rlawl4240@gmail.com>
2025-06-13 09:05:53 +0900

    feat: 회원 로그인 기본 설정 세팅
    태그: setting
    
    본문: 회원 로그인 기본 세팅

---
+++ user-service/src/main/java/com/gp/nut/auth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gp.nut.auth.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public class UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+}
