dcdb52859d40efec326a6c47914289d81fa8bb31
hyunwoo_lim <dla6963@naver.com>
2025-06-14 16:52:22 +0900

    fix: develop에서 실행시 오류가 생겨 다시 머지함
    태그: review update
    
    본인 브랜치에서 실행시 문제가 없으나 서버 연결을 위해 develop에 푸쉬

--- review-management/src/main/java/com/lhw/reviewmanagement/dto/ReviewDTO.java
+++ review-management/src/main/java/com/lhw/reviewmanagement/dto/ReviewDTO.java
@@ -1,12 +1,13 @@
 package com.lhw.reviewmanagement.dto;
 
-import com.lhw.reviewmanagement.Entity.Review;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ReviewDTO {
+
     private Long reviewId;
     private Long  scheduleId;   // schedule 테이블 고유 Id
     private Long userId ;     // user 테이블 고유 Id
