9cd7614a20f9c8a3f1d5a8f5f4945f702e112392
hyeblee <hyeb1224@naver.com>
2025-06-15 21:46:09 +0900

    refactor: Gathering 객체에 회식명 필드 추가
    - gatheringName 추가

--- schedule-service/src/main/java/com/gp/nut/schedule/entity/Gathering.java
+++ schedule-service/src/main/java/com/gp/nut/schedule/entity/Gathering.java
@@ -31,6 +31,8 @@ public class Gathering {
   @Id
   private Long id; // pk
 
+  private String gatheringName; // 회식명
+
   @Column(nullable = false)
   private Long bossId; // 회식 만든 사장의 id
 
@@ -63,6 +65,7 @@ public class Gathering {
   // Gathering을 Request로 변환
   public GatheringRequestDto toRequestDto() {
     return GatheringRequestDto.builder()
+        .gatheringName(gatheringName)
         .bossId(bossId)
         .Date(date)
         .confirmedLocationId(confirmedLocationId)
@@ -76,6 +79,7 @@ public class Gathering {
   public GatheringResponseDto toResponseDto() {
     return GatheringResponseDto.builder()
         .id(id)
+        .gatheringName(gatheringName)
         .bossId(bossId)
         .Date(date)
         .confirmedLocationId(confirmedLocationId)
