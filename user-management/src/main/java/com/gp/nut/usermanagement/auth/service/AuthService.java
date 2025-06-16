e150f6b237d858988c4df671e223c7b0ff043958
FerryLa <rlawl4240@gmail.com>
2025-06-16 14:20:18 +0900

    feat: 리프레시토큰, 로그아웃 기능 구현
    태그: 로그아웃, 리프레시토큰
    
    리프레시토큰, 로그아웃 구현
    트러블 : 로그아웃 기능 구현시 header에 succes토큰, body에 refresh토큰 넣어줘야함

--- user-management/src/main/java/com/gp/nut/usermanagement/auth/service/AuthService.java
+++ user-management/src/main/java/com/gp/nut/usermanagement/auth/service/AuthService.java
@@ -54,4 +54,49 @@ public class AuthService {
                 .build();
     }
 
+    public TokenResponse refreshToken(String providedRefreshToken) {
+        jwtTokenProvider.validateToken(providedRefreshToken);
+        String username = jwtTokenProvider.getUsernameFromJWT(providedRefreshToken);
+
+        RefreshToken storedToken = refreshTokenRepository.findById(username)
+                .orElseThrow(() -> new BadCredentialsException("사원 계정으로 조회되는 토큰 없음"));
+
+        if(!storedToken.getToken().equals(providedRefreshToken)) {
+            throw new BadCredentialsException("리프레시 토큰 일치하지 않음");
+        }
+
+        if(storedToken.getExpiryDate().before(new Date())) {
+            throw new BadCredentialsException("리프레시 토큰 유효시간 만료");
+        }
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new BadCredentialsException("해당 리프레시 토큰을 위한 계정 없음"));
+
+        String accessToken = jwtTokenProvider.createToken(user.getUsername(), user.getRole().name(), user.getId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(user.getUsername(), user.getRole().name(), user.getId());
+
+        RefreshToken tokenEntity = RefreshToken.builder()
+                .username(user.getUsername())
+                .token(refreshToken)
+                .expiryDate(
+                        new Date(System.currentTimeMillis()
+                                + jwtTokenProvider.getRefreshExpiration())
+                )
+                .build();
+
+        refreshTokenRepository.save(tokenEntity);
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+
+    }
+
+    // 리프레시 토큰 삭제하는 구문
+    public void logout(String refreshToken) {
+        jwtTokenProvider.validateToken(refreshToken);   // 토큰 검증 (토큰이 변조되지 않았는지 validateToken이 검증)
+        String username = jwtTokenProvider.getUsernameFromJWT(refreshToken);    // 토큰 자체를 기준으로 삭제
+        refreshTokenRepository.deleteById(username);
+    }
 }
