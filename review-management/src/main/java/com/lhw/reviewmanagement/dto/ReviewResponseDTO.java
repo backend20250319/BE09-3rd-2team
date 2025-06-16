6f6536030248963981c586e2a1a13ad2d0a4c961
hyunwoo_lim <dla6963@naver.com>
2025-06-16 09:18:44 +0900

    fix : review서비스 조회 할때 스케줄 ID나오게 수정
    태그: ResponseDTO 수정
    
    review서비스 조회시 스케줄 ID도 출력하게 코드 수정

--- review-management/src/main/java/com/lhw/reviewmanagement/dto/ReviewResponseDTO.java
+++ review-management/src/main/java/com/lhw/reviewmanagement/dto/ReviewResponseDTO.java
@@ -8,10 +8,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ReviewResponseDTO{
     private Long reviewId ;
+    private Long scheduleId ;
     private String comment;
 
     public ReviewResponseDTO(Review review){
         this.reviewId = review.getReviewId();
+        this.scheduleId = review.getScheduleId();
         this.comment = review.getComment();
     }
 
